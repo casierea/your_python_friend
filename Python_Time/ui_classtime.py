711b63b765c4d570f7d00a4466b9e0969156b852
Unknown <dancingmongoose@gmail.com>
2020-11-18 15:52:06 -0700

    UI WIP

--- Python_Time/ui_classtime.py
+++ Python_Time/ui_classtime.py
@@ -1,14 +1,17 @@
 import maya.cmds as cmds
-my_window = 'cr_cool_tool_window'
+my_window = 'CasieToolWindow2'
 #formlayout
 #look under controls for button,sliders,what you will need.
 
-if cmds.window(my_window, exists=True):
-    cmds.deleteUI(my_window)
+
+
+#if cmds.window(my_window, exists=True):
+    #cmds.deleteUI(my_window)
 
 my_window = cmds.window(my_window,
                         title='Super Cool Tool Window',
                         widthHeight=(200,200)) #creates window. Grandaddy. Main parent
+
 
 cmds.columnLayout()
 cmds.button()
@@ -22,22 +25,25 @@ cmds.button()
 cmds.button()
 cmds.button()
 
-cmds.rowColumnLayout(numberOfColums=3)
+cmds.rowColumnLayout(numberOfColumns=3)
 cmds.button()
 cmds.button()
 cmds.button()
 
-cmds.showWindow(my_window)
 
 
 rc_layout = cmds.rowColumnLayout(parent=my_window,
-                     numberOfColumns=3)
+                                 numberOfColumns=3)
+
 col_layout = cmds.columnLayout(parent=my_window)
 cmds.button(parent=rc_layout)
 cmds.button(parent=rc_layout)
 cmds.button(parent=rc_layout)
+
 cmds.button(parent=col_layout)
 cmds.button(parent=col_layout)
 cmds.button(parent=col_layout)
+
+cmds.showWindow(my_window)
 
 #rename tool. Window- for a string. hit  button and do somthing with it.
