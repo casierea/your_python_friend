b55832f2af7c75654eb45bde6e4f82da74af9630
Unknown <dancingmongoose@gmail.com>
2020-12-08 16:48:50 -0700

    Window_works_ui back to basic- safe push

---
+++ Python_Time/Ui_Window_Renamer.py
@@ -0,0 +1,22 @@
+
+import maya.cmds as cmds
+class RenamerUI():
+    def__init__(self)
+        self.my_window = 'CasieToolWindow2'
+    def create(self):
+        self.delete()
+
+    self.my_window = cmds.window(my_window,
+                        title='Super Cool Tool Window',
+                        widthHeight=(200,200)) #creates window. Grandaddy. Main parent
+    self.col_layout = cmds.columnLayout(parent=self.my_window,
+                                        adjustableColumn=True)
+    self.name_field = cmds.textField(parent=self.col_layout,
+                                     placeholderText='Name of new object...')
+
+    cmds.button(parent=self.col_layout, label='Sphere', c='createSphere()')
+    cmds.button(parent=self.col_layout, label='Cubee', c='cmds.polyCube()')
+    cmds.button(parent=self.col_layout, label='Torus', c='cmds.polyTorus()')
+    cmds.button(parent=self.col_layout, label='Print field',
+                c='print cmds.textField(name_field, q=True, text=True)')
+    cmds.ShowWindow(self.my_window)
